98b0303da90e643df0b3e56ca97d9ed89e3a4012
Sara Kodehode <sarah88.kodehode@gmail.com>
2024-02-23 09:00:17 +0100

    displaying function (name and films)

--- main.js
+++ main.js
@@ -3,37 +3,31 @@ const listContainer = document.createElement("ul");
 // listContainer.className("list-container");
 container.append(listContainer);
 
-async function getQuizData() {
+async function getCharacterData() {
   try {
     const result = await fetch("https://api.disneyapi.dev/character");
     const data = await result.json();
-    // console.log(data);
-    console.log(data);
     display(data);
   } catch (error) {
     console.log(error.message);
   }
 }
-getQuizData();
+getCharacterData();
 
 function display(data) {
-  data.data.forEach((e) => {
+  data.data.forEach((character) => {
     const charElem = document.createElement("li");
-    let charName = document.createElement("h2");
-    let charFilms = document.createElement("span");
-    // console.log(e.name);
+    const charName = document.createElement("h2");
 
-    e.films.forEach((film) => {
-      if (!film) {
-        console.log(film);
+    charName.textContent = character.name;
+    charElem.append(charName);
 
-        charFilms = film;
-      }
+    character.films.forEach((film) => {
+      let charFilms = document.createElement("p");
+      charFilms.textContent = film;
+      charElem.append(charFilms);
     });
-    // charName = e.name;
-    // console.log(charName);
-    // charFilms = e.films;
-    charElem.append(charName, charFilms);
+
     listContainer.append(charElem);
   });
 }
